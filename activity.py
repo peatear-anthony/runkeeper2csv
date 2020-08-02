c1ae0e00d2dfe52a1078ecf1b402b77f124fa344
Peter Sunga <44995752+peatear-anthony@users.noreply.github.com>
2020-08-03 02:16:24 +0900

    added splits and elevatation to activity

--- activity.py
+++ activity.py
@@ -1,11 +1,14 @@
 import os
+import datetime
 import gpxpy
 import gpxpy.gpx
 import logging 
 import logging.config
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from math import sin, cos, sqrt, atan2, radians
+from statistics import mean
 
 
 class Activity():
@@ -15,13 +18,21 @@ class Activity():
     def __init__(self, path):
         self.path = path
         self.logger = logging.getLogger(__name__)
+        self.total_distance = None
+        self.duration = None
+        self.avg_pace = None
+        self.date = None
+
         # Start private methods 
         self._load_gpx()
         self._segment_df = self._get_segment_df()
-        print(self._segment_df)
-        self._segment_df.plot(x='t_cumsum', y='avg_pace')
+        self._get_activity_values()
+        self._get_splits_df()
+        '''
+        self._segment_df.plot(x='t_cumsum', y='avg_elevation')
         plt.show()
-    
+        '''
+
     def _load_gpx(self):
         #  Load gpx file from path
         if self.path.split('.')[-1] != 'gpx':
@@ -36,25 +47,45 @@ class Activity():
         # Return dataframe with activity information
         t_list = []
         d_list = []
+        e_list = []
+
         for segment in self.gpx.tracks[0].segments:
             t = [float((segment.points[i + 1].time - segment.points[i].time).seconds)
                 for i in range(len(segment.points) - 1)]
 
             d = [self._calc_distance(segment.points[i + 1], segment.points[i]) 
                 for i in range(len(segment.points) - 1)]
-            
+
+            e = [mean([segment.points[i + 1].elevation , segment.points[i].elevation])
+                for i in range(len(segment.points) - 1)]
+
             t_list.extend(t)
             d_list.extend(d)
+            e_list.extend(e)
 
-        df = pd.DataFrame(zip(d_list, t_list), columns=['delta_d', 'delta_t'])
+        df = pd.DataFrame(zip(d_list, t_list, e_list), columns=['delta_d', 'delta_t', 'avg_elevation'])
         df = df.drop(df[df.delta_t > 6].index)
         df['pace'] = self._calc_pace(df['delta_d'], df['delta_t'])
         df['d_cumsum'] = df['delta_d'].cumsum()
         df['t_cumsum'] = df['delta_t'].cumsum()
         df['avg_pace'] = self._calc_pace(df['d_cumsum'], df['t_cumsum'])
 
-
         return df
+
+
+    def _get_splits_df(self):
+        # Calculate the splits for the activity then returns the results in a df
+
+        indexes = [abs(self._segment_df['d_cumsum'] - km * 1e3).idxmin() 
+        for km in range(1, int(self.total_distance / 1e3) + 1)]
+        indexes.insert(0, 0)
+        print(indexes)
+        
+        splits = [self._segment_df.iloc[indexes[i + 1]].t_cumsum -self._segment_df.iloc[indexes[i]].t_cumsum
+                    for i in range(len(indexes) - 1)]
+        splits = [str(datetime.timedelta(seconds=split)) for split in splits]
+                
+        return splits 
 
 
     def _calc_distance(self, c1, c2):
@@ -75,6 +106,12 @@ class Activity():
     def _calc_pace(self, d, t):
         # Given d and t will return the pace in min/km
         return (1000/(d/t))/60.0
+    
+    def _get_activity_values(self):
+        # get the activity_values from the segment df
+        self.total_distance = self._segment_df.tail(1)['d_cumsum'].values[0]
+        self.duration = self._segment_df.tail(1)['t_cumsum'].values[0]
+        self.avg_pace = self._segment_df.tail(1)['avg_pace'].values[0]
 
 if __name__ == "__main__":
     logger_path = os.path.join(os.path.dirname(__file__), 'log', 'logging.conf')
@@ -83,4 +120,5 @@ if __name__ == "__main__":
 
     file_path = os.path.join("data", "2020-07-07-190130.gpx")
     activity = Activity(file_path)
+    print(activity.total_distance)
 
