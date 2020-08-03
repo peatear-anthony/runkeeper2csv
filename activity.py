e739617b113265cf528652243d56f3e06def3497
Peter Sunga <44995752+peatear-anthony@users.noreply.github.com>
2020-08-03 10:05:58 +0900

    update: splits for accuracy

--- activity.py
+++ activity.py
@@ -22,6 +22,8 @@ class Activity():
         self.duration = None
         self.avg_pace = None
         self.date = None
+        self.start_time = None
+        self.end_time = None
 
         # Start private methods 
         self._load_gpx()
@@ -29,7 +31,7 @@ class Activity():
         self._get_activity_values()
         self._get_splits_df()
         '''
-        self._segment_df.plot(x='t_cumsum', y='avg_elevation')
+        self._segment_df.plot(x='t_cumsum', y='avg_pace')
         plt.show()
         '''
 
@@ -62,9 +64,9 @@ class Activity():
             t_list.extend(t)
             d_list.extend(d)
             e_list.extend(e)
-
+            
         df = pd.DataFrame(zip(d_list, t_list, e_list), columns=['delta_d', 'delta_t', 'avg_elevation'])
-        df = df.drop(df[df.delta_t > 6].index)
+        df = df.drop(df[df.delta_t > 15].index)
         df['pace'] = self._calc_pace(df['delta_d'], df['delta_t'])
         df['d_cumsum'] = df['delta_d'].cumsum()
         df['t_cumsum'] = df['delta_t'].cumsum()
@@ -75,16 +77,21 @@ class Activity():
 
     def _get_splits_df(self):
         # Calculate the splits for the activity then returns the results in a df
-
         indexes = [abs(self._segment_df['d_cumsum'] - km * 1e3).idxmin() 
         for km in range(1, int(self.total_distance / 1e3) + 1)]
         indexes.insert(0, 0)
-        print(indexes)
-        
+                
         splits = [self._segment_df.iloc[indexes[i + 1]].t_cumsum -self._segment_df.iloc[indexes[i]].t_cumsum
                     for i in range(len(indexes) - 1)]
-        splits = [str(datetime.timedelta(seconds=split)) for split in splits]
-                
+        splits = {float(km): str(datetime.timedelta(seconds=split)) 
+                    for km, split in enumerate(splits, start=1)}
+
+        # Add last split
+        last_split = round((self._segment_df.iloc[-1].t_cumsum - self._segment_df.iloc[indexes[-1]].t_cumsum) \
+            / (self.total_distance / 1e3 - len(splits)), 0)
+        splits[round(self.total_distance/1e3, 1)] = str(datetime.timedelta(seconds=last_split))
+        
+        print(splits)
         return splits 
 
 
@@ -105,20 +112,23 @@ class Activity():
 
     def _calc_pace(self, d, t):
         # Given d and t will return the pace in min/km
-        return (1000/(d/t))/60.0
+        return (1e3/(d/t))/60.0
     
     def _get_activity_values(self):
         # get the activity_values from the segment df
         self.total_distance = self._segment_df.tail(1)['d_cumsum'].values[0]
         self.duration = self._segment_df.tail(1)['t_cumsum'].values[0]
-        self.avg_pace = self._segment_df.tail(1)['avg_pace'].values[0]
+        self.avg_pace = str(datetime.timedelta(
+            seconds = round(self._segment_df.tail(1)['avg_pace'].values[0] * 60)))
+        self.start_time = self.gpx.tracks[0].segments[0].points[0].time
+        self.end_time = self.gpx.tracks[0].segments[-1].points[-1].time
 
 if __name__ == "__main__":
     logger_path = os.path.join(os.path.dirname(__file__), 'log', 'logging.conf')
     logging.config.fileConfig(logger_path)
     logger = logging.getLogger(__name__)
 
-    file_path = os.path.join("data", "2020-07-07-190130.gpx")
+    file_path = os.path.join("data", "2020-07-26-183202.gpx")
     activity = Activity(file_path)
-    print(activity.total_distance)
+
 
